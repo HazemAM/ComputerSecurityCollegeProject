748760178f23e1c182a9bf266c8f980b35ae7089
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-04-27 06:52:14 +0200

    MainWindow: Encryption

--- ComputerSecurity/MainWindow.Designer.cs
+++ ComputerSecurity/MainWindow.Designer.cs
@@ -28,20 +28,180 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
+			this.rdioCeasar = new System.Windows.Forms.RadioButton();
+			this.rdioPlayfair = new System.Windows.Forms.RadioButton();
+			this.rdioVigenereRe = new System.Windows.Forms.RadioButton();
+			this.rdioVigenereAuto = new System.Windows.Forms.RadioButton();
+			this.rdioRC4 = new System.Windows.Forms.RadioButton();
+			this.rdioMD5 = new System.Windows.Forms.RadioButton();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.txtPlaintext = new System.Windows.Forms.TextBox();
+			this.txtKey = new System.Windows.Forms.TextBox();
+			this.btnEncrypt = new System.Windows.Forms.Button();
+			this.txtCiphertext = new System.Windows.Forms.TextBox();
+			this.btnDecrypt = new System.Windows.Forms.Button();
+			this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// rdioCeasar
+			// 
+			this.rdioCeasar.AutoSize = true;
+			this.rdioCeasar.Checked = true;
+			this.rdioCeasar.Location = new System.Drawing.Point(10, 23);
+			this.rdioCeasar.Name = "rdioCeasar";
+			this.rdioCeasar.Size = new System.Drawing.Size(58, 17);
+			this.rdioCeasar.TabIndex = 0;
+			this.rdioCeasar.TabStop = true;
+			this.rdioCeasar.Text = "Ceasar";
+			this.rdioCeasar.UseVisualStyleBackColor = true;
+			// 
+			// rdioPlayfair
+			// 
+			this.rdioPlayfair.AutoSize = true;
+			this.rdioPlayfair.Location = new System.Drawing.Point(98, 23);
+			this.rdioPlayfair.Name = "rdioPlayfair";
+			this.rdioPlayfair.Size = new System.Drawing.Size(59, 17);
+			this.rdioPlayfair.TabIndex = 1;
+			this.rdioPlayfair.Text = "Playfair";
+			this.rdioPlayfair.UseVisualStyleBackColor = true;
+			// 
+			// rdioVigenereRe
+			// 
+			this.rdioVigenereRe.AutoSize = true;
+			this.rdioVigenereRe.Location = new System.Drawing.Point(187, 23);
+			this.rdioVigenereRe.Name = "rdioVigenereRe";
+			this.rdioVigenereRe.Size = new System.Drawing.Size(108, 17);
+			this.rdioVigenereRe.TabIndex = 2;
+			this.rdioVigenereRe.Text = "Vigenere: Repeat";
+			this.rdioVigenereRe.UseVisualStyleBackColor = true;
+			// 
+			// rdioVigenereAuto
+			// 
+			this.rdioVigenereAuto.AutoSize = true;
+			this.rdioVigenereAuto.Location = new System.Drawing.Point(325, 23);
+			this.rdioVigenereAuto.Name = "rdioVigenereAuto";
+			this.rdioVigenereAuto.Size = new System.Drawing.Size(95, 17);
+			this.rdioVigenereAuto.TabIndex = 3;
+			this.rdioVigenereAuto.Text = "Vigenere: Auto";
+			this.rdioVigenereAuto.UseVisualStyleBackColor = true;
+			// 
+			// rdioRC4
+			// 
+			this.rdioRC4.AutoSize = true;
+			this.rdioRC4.Location = new System.Drawing.Point(10, 46);
+			this.rdioRC4.Name = "rdioRC4";
+			this.rdioRC4.Size = new System.Drawing.Size(46, 17);
+			this.rdioRC4.TabIndex = 4;
+			this.rdioRC4.Text = "RC4";
+			this.rdioRC4.UseVisualStyleBackColor = true;
+			// 
+			// rdioMD5
+			// 
+			this.rdioMD5.AutoSize = true;
+			this.rdioMD5.Location = new System.Drawing.Point(98, 46);
+			this.rdioMD5.Name = "rdioMD5";
+			this.rdioMD5.Size = new System.Drawing.Size(48, 17);
+			this.rdioMD5.TabIndex = 5;
+			this.rdioMD5.Text = "MD5";
+			this.rdioMD5.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.rdioMD5);
+			this.groupBox1.Controls.Add(this.rdioRC4);
+			this.groupBox1.Controls.Add(this.rdioCeasar);
+			this.groupBox1.Controls.Add(this.rdioVigenereRe);
+			this.groupBox1.Controls.Add(this.rdioVigenereAuto);
+			this.groupBox1.Controls.Add(this.rdioPlayfair);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(434, 76);
+			this.groupBox1.TabIndex = 7;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Encryption Algorithm";
+			// 
+			// txtPlaintext
+			// 
+			this.txtPlaintext.Location = new System.Drawing.Point(18, 129);
+			this.txtPlaintext.Multiline = true;
+			this.txtPlaintext.Name = "txtPlaintext";
+			this.txtPlaintext.Size = new System.Drawing.Size(171, 46);
+			this.txtPlaintext.TabIndex = 8;
+			this.toolTip.SetToolTip(this.txtPlaintext, "Plaintext");
+			// 
+			// txtKey
+			// 
+			this.txtKey.Location = new System.Drawing.Point(144, 106);
+			this.txtKey.Name = "txtKey";
+			this.txtKey.Size = new System.Drawing.Size(171, 20);
+			this.txtKey.TabIndex = 9;
+			this.toolTip.SetToolTip(this.txtKey, "Key");
+			// 
+			// btnEncrypt
+			// 
+			this.btnEncrypt.Location = new System.Drawing.Point(191, 128);
+			this.btnEncrypt.Name = "btnEncrypt";
+			this.btnEncrypt.Size = new System.Drawing.Size(75, 25);
+			this.btnEncrypt.TabIndex = 10;
+			this.btnEncrypt.Text = "Encrypt »";
+			this.btnEncrypt.UseVisualStyleBackColor = true;
+			this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+			// 
+			// txtCiphertext
+			// 
+			this.txtCiphertext.Location = new System.Drawing.Point(268, 129);
+			this.txtCiphertext.Multiline = true;
+			this.txtCiphertext.Name = "txtCiphertext";
+			this.txtCiphertext.Size = new System.Drawing.Size(171, 46);
+			this.txtCiphertext.TabIndex = 11;
+			this.toolTip.SetToolTip(this.txtCiphertext, "Ciphertext");
+			// 
+			// btnDecrypt
+			// 
+			this.btnDecrypt.Location = new System.Drawing.Point(191, 152);
+			this.btnDecrypt.Name = "btnDecrypt";
+			this.btnDecrypt.Size = new System.Drawing.Size(75, 24);
+			this.btnDecrypt.TabIndex = 12;
+			this.btnDecrypt.Text = "« Decrypt";
+			this.btnDecrypt.UseVisualStyleBackColor = true;
 			// 
 			// MainWindow
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.ClientSize = new System.Drawing.Size(458, 192);
+			this.Controls.Add(this.btnDecrypt);
+			this.Controls.Add(this.txtCiphertext);
+			this.Controls.Add(this.btnEncrypt);
+			this.Controls.Add(this.txtKey);
+			this.Controls.Add(this.txtPlaintext);
+			this.Controls.Add(this.groupBox1);
 			this.Name = "MainWindow";
-			this.Text = "MainWindow";
+			this.Text = "Encryption and Decryption";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.RadioButton rdioCeasar;
+		private System.Windows.Forms.RadioButton rdioPlayfair;
+		private System.Windows.Forms.RadioButton rdioVigenereRe;
+		private System.Windows.Forms.RadioButton rdioVigenereAuto;
+		private System.Windows.Forms.RadioButton rdioRC4;
+		private System.Windows.Forms.RadioButton rdioMD5;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox txtPlaintext;
+		private System.Windows.Forms.TextBox txtKey;
+		private System.Windows.Forms.Button btnEncrypt;
+		private System.Windows.Forms.TextBox txtCiphertext;
+		private System.Windows.Forms.Button btnDecrypt;
+		private System.Windows.Forms.ToolTip toolTip;
 	}
 }
 
--- ComputerSecurity/MainWindow.cs
+++ ComputerSecurity/MainWindow.cs
@@ -26,5 +26,47 @@ namespace ComputerSecurity
 
 			result = MD5.getHash("Hazem AbuMostafa").ToLower();
 		}
+
+		private void btnEncrypt_Click(object sender, EventArgs e)
+		{
+			string plaintext = txtPlaintext.Text,
+				   key = txtKey.Text;
+			int intKey = 0;
+
+			/* CATCHING ERRORS */
+			if(rdioCeasar.Checked)
+				try{
+					intKey = int.Parse(key);
+				} catch{
+					MessageBox.Show(this, "Key must be integer in Ceasar.", "Wrong key",
+						MessageBoxButtons.OK, MessageBoxIcon.Information);
+					return;
+				}
+			else if (rdioPlayfair.Checked){
+				//TODO: Check for out-of-alphabet characters in key.
+			}
+
+			if(plaintext.Length == 0)
+				return;
+			//TODO: Check when (key.Length == 0) for algorithms that require that.
+
+			/* ENCRYTING */
+			if(rdioCeasar.Checked)
+				txtCiphertext.Text = Ceasar.encrypt(plaintext, intKey);
+			else if(rdioPlayfair.Checked)
+				txtCiphertext.Text = Playfair.encrypt(plaintext, key);
+			else if(rdioVigenereRe.Checked){
+				txtKey.Text = Vigenere.formulateKey(plaintext, key, VigenereType.REPEATING_KEY);
+				txtCiphertext.Text = Vigenere.encrypt(plaintext, txtKey.Text);
+			}
+			else if(rdioVigenereAuto.Checked){
+				txtKey.Text = Vigenere.formulateKey(plaintext, key, VigenereType.AUTO_KEY);
+				txtCiphertext.Text = Vigenere.encrypt(plaintext, txtKey.Text);
+			}
+			else if(rdioRC4.Checked)
+				txtCiphertext.Text = RC4.encrypt(plaintext, key);
+			else if(rdioMD5.Checked)
+				txtCiphertext.Text = MD5.getHash(plaintext).ToLower();
+		}
 	}
 }
