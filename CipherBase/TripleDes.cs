384041874224cd8a7e20f9ee1a79346699ce4969
chaosagex <hk2nasr@gmail.com>
2015-05-06 03:57:09 +0200

    meh

--- CipherBase/TripleDes.cs
+++ CipherBase/TripleDes.cs
@@ -20,15 +20,15 @@ namespace ComputerSecurity
         public string encrypt()
         {
             Des D1 = new Des(keys[0], input,mode);
-            Des D2 = new Des(keys[1], D1.Encode(), mode);
-            Des D3 = new Des(keys[2], D2.decode(), mode);
+            Des D2 = new Des(keys[1], D1.Encode().Replace("-",""), mode);
+            Des D3 = new Des(keys[2], D2.decode().Replace("-",""), mode);
             return D3.Encode();
         }
         public string decrypt()
         {
             Des D3 = new Des(keys[2], input, mode);
-            Des D2 = new Des(keys[1], D3.decode(), mode);
-            Des D1 = new Des(keys[0], D2.Encode(), mode);
+            Des D2 = new Des(keys[1], D3.decode().Replace("-",""), mode);
+            Des D1 = new Des(keys[0], D2.Encode().Replace("-", ""), mode);
             return D1.decode();
         }
     }
